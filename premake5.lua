5ff6bcef7a0a07acbe9df5c1b912fd9b77163b55
Damon S. Green II <damonsgreen2@gmail.com>
2025-12-03 14:57:14 -0500

    setup premake

---
+++ premake5.lua
@@ -0,0 +1,117 @@
+workspace "Vesper"
+	architecture "x64"
+
+	configurations 
+	{ 
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Vesper"
+	location "Vesper"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "Off"
+		systemversion "latest"
+
+    buildoptions { "/utf-8" }
+
+
+	defines
+	{
+		"VZ_PLATFORM_WINDOWS", 
+		"VZ_BUILD_DLL" 
+	}
+
+	postbuildcommands
+	{
+		("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+	}
+
+	filter "configurations:Debug"
+		defines "VZ_DEBUG"
+			symbols "On"
+
+	filter "configurations:Release"
+		defines "VZ_RELEASE"
+			optimize "On"
+
+	filter "configurations:Dist"
+		defines "VZ_DIST"
+			optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files 
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Vesper/vendor/spdlog/include",
+		"Vesper/src"
+	}
+
+	links
+	{
+		"Vesper"
+	}
+
+	dependson
+    {
+        "Vesper"
+    }
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "Off"
+		systemversion "latest"
+
+    buildoptions { "/utf-8" }
+
+
+		defines
+		{
+			"VZ_PLATFORM_WINDOWS"
+		}
+
+		filter "configurations:Debug"
+			defines "VZ_DEBUG"
+				symbols "On"
+
+		filter "configurations:Release"
+			defines "VZ_RELEASE"
+				optimize "On"
+
+		filter "configurations:Dist"
+			defines "VZ_DIST"
+				optimize "On"
